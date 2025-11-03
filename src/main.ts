562f962fe60ebf5f58009daf82962774082e151d
joaoobraz <joaobraz.ofc@gmail.com>
2025-11-03 14:17:43 -0300

    att p100

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  // Inicializa o NestJS sem o objeto { cors: true }
+  const app = await NestFactory.create(AppModule); 
+
+  // Habilitar CORS explicitamente para *QUALQUER* origem.
+  // Isso garante que o CORS não está bloqueando, mesmo se o Vercel for a origem.
+  // ATENÇÃO: Use 'origin: "*"' apenas em ambientes de teste.
+  app.enableCors({
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
   const port = Number(process.env.PORT) || 3000;
 
-  // Importante em container
+  // Importante em container para ouvir em todas as interfaces
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
