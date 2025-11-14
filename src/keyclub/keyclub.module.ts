9b31dee9300a3ac8bc63f7bc4e53a1f3c495e2e8
joaoobraz <joaobraz.ofc@gmail.com>
2025-11-14 03:33:00 -0300

    fix: remove keyclub.controller duplicado e corrige URL webhook

--- src/keyclub/keyclub.module.ts
+++ src/keyclub/keyclub.module.ts
@@ -1,12 +1,9 @@
 // src/keyclub/keyclub.module.ts
 import { Module } from '@nestjs/common';
 import { KeyclubService } from './keyclub.service';
-import { KeyclubController } from './keyclub.controller';
-import { WebhooksModule } from '../webhooks/webhooks.module';
 
 @Module({
-  imports: [WebhooksModule],
-  controllers: [KeyclubController],
+  controllers: [],
   providers: [KeyclubService],
   exports: [KeyclubService],
 })
