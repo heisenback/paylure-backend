98082738a0dd403c62cf5442eb8266d8c9286a75
joaoobraz <joaobraz.ofc@gmail.com>
2025-11-16 23:03:25 -0300

    fix: admin module exports

--- src/withdrawal/withdrawal.module.ts
+++ src/withdrawal/withdrawal.module.ts
@@ -4,14 +4,16 @@ import { WithdrawalController } from './withdrawal.controller';
 import { WithdrawalService } from './withdrawal.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { KeyclubModule } from 'src/keyclub/keyclub.module';
+import { AdminModule } from 'src/admin/admin.module'; // 👈 ADICIONE ESTA LINHA!
 
 @Module({
   imports: [
-    PrismaModule, 
-    KeyclubModule, // Necessário para usar o KeyclubService
+    PrismaModule,
+    KeyclubModule,
+    AdminModule, // 👈 ADICIONE ESTA LINHA!
   ],
   controllers: [WithdrawalController],
   providers: [WithdrawalService],
-  exports: [WithdrawalService], // ✅ CORREÇÃO: Exporta o WithdrawalService
+  exports: [WithdrawalService],
 })
 export class WithdrawalModule {}
