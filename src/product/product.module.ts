147ec2ac272c0eca9aec31fb206918c4f58039d0
joaoobraz <joaobraz.ofc@gmail.com>
2025-12-18 06:00:48 -0300

    atualizar tudo

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,11 +1,15 @@
 // src/product/product.module.ts
 import { Module } from '@nestjs/common';
-import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { ProductController } from './product.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { MailModule } from 'src/mail/mail.module'; // ✅ 1. IMPORTAR O MÓDULO DE EMAIL
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule, 
+    MailModule // ✅ 2. ADICIONAR AQUI NOS IMPORTS
+  ],
   controllers: [ProductController],
   providers: [ProductService],
   exports: [ProductService],
