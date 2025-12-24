538ecc473c7b395c8f7c3e754731d1c9e87fa860
joaoobraz <joaobraz.ofc@gmail.com>
2025-12-24 15:10:25 -0300

    atualização necessaria

--- src/xflow/xflow.module.ts
+++ src/xflow/xflow.module.ts
@@ -1,68 +1,9 @@
-// src/app.module.ts
+// src/xflow/xflow.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { ConfigModule } from '@nestjs/config';
-
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
-import { DepositModule } from './deposit/deposit.module';
-import { WithdrawalModule } from './withdrawal/withdrawal.module';
-import { TransactionsModule } from './transactions/transactions.module';
-import { WebhooksModule } from './webhooks/webhooks.module';
-import { KeyclubModule } from './keyclub/keyclub.module';
-import { ProductModule } from './product/product.module';
-import { MarketplaceModule } from './marketplace/marketplace.module';
-import { AffiliateModule } from './affiliate/affiliate.module';
-import { SalesModule } from './sales/sales.module';
-import { MembershipModule } from './membership/membership.module';
-import { SubscriptionModule } from './subscription/subscription.module';
-import { MerchantModule } from './merchant/merchant.module';
-import { PaymentLinkModule } from './payment-link/payment-link.module';
-import { ReportModule } from './report/report.module';
-import { PublicApiModule } from './api/public-api.module';
-import { SocketModule } from './gateway/socket.module';
-import { PushNotificationModule } from './push-notification/push-notification.module';
-import { AdminModule } from './admin/admin.module';
-import { CheckoutModule } from './checkout/checkout.module';
-import { PixelsModule } from './pixels/pixels.module';
-
-import { MemberAreaModule } from './member-area/member-area.module';
-
-// ✅ NOVO
-import { XflowModule } from './xflow/xflow.module';
+import { XflowService } from './xflow.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
-    }),
-    PrismaModule,
-    AuthModule,
-    DepositModule,
-    WithdrawalModule,
-    TransactionsModule,
-    WebhooksModule,
-    KeyclubModule,
-    XflowModule, // ✅ ADICIONADO
-    ProductModule,
-    MarketplaceModule,
-    AffiliateModule,
-    SalesModule,
-    MembershipModule,
-    SubscriptionModule,
-    MerchantModule,
-    PaymentLinkModule,
-    ReportModule,
-    PublicApiModule,
-    SocketModule,
-    PushNotificationModule,
-    AdminModule,
-    CheckoutModule,
-    PixelsModule,
-    MemberAreaModule,
-  ],
-  controllers: [AppController],
-  providers: [],
+  providers: [XflowService],
+  exports: [XflowService],
 })
-export class AppModule {}
+export class XflowModule {}
